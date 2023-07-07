0b77fc259f783e5244bbbc76c16e89573a940a7d
unknown <aisha.fathy123@gmail.com>
2023-07-08 01:39:01 +0300

    fix error

--- script.groovy
+++ script.groovy
@@ -4,18 +4,18 @@ def buildJar(){
 }
 
 def buildImage(String imageName){
-    sh "docker build -t $imageName ."
+    sh "sudo docker build -t $imageName ."
 }
 
 def dockerLogin(){
     echo "building the docker image..."
     WithCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]){
-        sh "echo $PASS | docker login -u $USER --password-stdin"
+        sh "echo $PASS | sudo docker login -u $USER --password-stdin"
     }
 }
 
 def dockerpush(String imageName){
     echo "pushing the docker image..."
-    sh "docker push $imageName"
+    sh "sudo docker push $imageName"
 }
 return this
