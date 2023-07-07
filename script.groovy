edd287081e8333d8be5bc870d8fdc5e331d6a5e7
unknown <aisha.fathy123@gmail.com>
2023-07-07 19:45:10 +0300

    save changes

---
+++ script.groovy
@@ -0,0 +1,20 @@
+def buildJar(){
+    echo "building the application..."
+    sh "mvn package"
+}
+
+def buildImage(String imageName){
+    sh "docker build $imageName ."
+}
+
+def dockerLogin(){
+    echo "building the docker image..."
+    WithCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]){
+        sh "echo $PASS | docker login -u $USER --password-stdin"
+    }
+}
+
+def dockerpush(String imageName){
+    echo "pushing the docker image..."
+    sh "docker push $imageName"
+}
---
+++ server-cmd.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+export DOCKER_PWD=$3
+echo $DOCKER_PWD | docker login -u $DOCKER_USER --password-stdin
+docker-compose -f docker-compose.yaml up --detach
+echo "success"
