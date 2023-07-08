301910b83744c2b1308a674812589220221baadb
unknown <aisha.fathy123@gmail.com>
2023-07-08 03:14:27 +0300

    fix error

--- terraform/main.tf
+++ terraform/main.tf
@@ -26,7 +26,7 @@ resource "aws_internet_gateway" "myapp-igw" {
 }
 
 resource "aws_default_route_table" "mair-rtb" {
-    default-route-table-id = aws_vpc.myapp-vpc.default_route_table_id
+    default_route_table_id = aws_vpc.myapp-vpc.default_route_table_id
     route {
         cidr_block = "0.0.0.0/0"
         gateway_id = aws_internet_gateway.myapp-igw
