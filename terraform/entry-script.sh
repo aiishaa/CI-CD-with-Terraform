9b1105196a58ea32e3a9152b231df35d04c2bf9f
unknown <aisha.fathy123@gmail.com>
2023-07-08 04:37:44 +0300

    let the ec2 instance login to dockerHub

--- terraform/entry-script.sh
+++ terraform/entry-script.sh
@@ -5,4 +5,4 @@ sudo usermod -aG docker ec2-user
 
 # install docker compose
 sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-sudo chmod +X /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
