6077a8fc3916e885728efffc133965b1384de053
unknown <aisha.fathy123@gmail.com>
2023-07-08 03:37:58 +0300

    fix error

--- terraform/main.tf
+++ terraform/main.tf
@@ -25,11 +25,11 @@ resource "aws_internet_gateway" "myapp-igw" {
     }
 }
 
-resource "aws_default_route_table" "mair-rtb" {
+resource "aws_default_route_table" "main-rtb" {
     default_route_table_id = aws_vpc.myapp-vpc.default_route_table_id
     route {
         cidr_block = "0.0.0.0/0"
-        gateway_id = aws_internet_gateway.myapp-igw
+        gateway_id = aws_internet_gateway.myapp-igw.id
     }
     tags = {
         Name: "${var.env_prefix}-main-rtb"
